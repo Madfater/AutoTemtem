12b0f0514da010814febd4955f2b079baee9f3e8
Madfater <ericleeddd@gmail.com>
2022-10-02 18:22:26 +0800

    add test mode

--- Autotemtem.py
+++ Autotemtem.py
@@ -123,12 +123,10 @@ class AutoTemtem():
         self.script.join()
         self.keyboard.stop()
 
-
 if __name__=='__main__':
     mode=int(input("1.色違尋找模式\n2.自動練等模式\n3.每周釋放\n4.測試補卡功能\n請選擇模式:"))
     if mode ==4:
         temtem_utils.buy_temcard()
     else:
         AutoTemtem(k,k_exit,mode).start()
-    
     
